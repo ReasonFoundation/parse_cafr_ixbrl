186ae99c70047975c4064704df802e14593189d6
Marc Joffe <marc@publicsectorcredit.org>
2019-01-18 18:09:50 -0800

    Updated config file and getix to use it

--- getix.py
+++ getix.py
@@ -20,24 +20,49 @@ urls = ['https://xbrlus.github.io/cafr/samples/3/Alexandria-2018-Statements.htm'
         'https://xbrlus.github.io/cafr/samples/4/FallsChurch-2018-Statements.htm',
         'https://xbrlus.github.io/cafr/samples/5/Loudoun-2018-Statements.htm',
         'https://xbrlus.github.io/cafr/samples/6/ga-20190116.htm',
-        'https://xbrlus.github.io/cafr/samples/1/StPete_StmtNetPos_iXBRL_20190116.htm',
-        'https://xbrlus.github.io/cafr/samples/2/VABeach_StmtNetPos_iXBRL_20190116.htm',
         'https://xbrlus.github.io/cafr/samples/7/ut-20190117.htm']
 
 
 # ## Libraries
 # **BeautifulSoup**: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 
-# In[2]:
-
-
 import requests
+from pandas import Series, DataFrame
 import pandas as pd
-
 import re
 
 
-# In[3]:
+def config_fields(path = 'config.csv'):
+    ''' 
+    If the config CSV file exists, it is used to determine what to output.
+    If config file doesn't exist, raises exception.
+    
+    CSV Format:
+    
+    Output Field Name,Input Field Names (a;b;c)
+    Document Title,cafr:DocumentTitle
+    
+    Returns a dictionary with the output field name as the key and a list of input fields as the value.
+    
+    {'Document Title': ['cafr:DocumentTitle'],
+     'Name of Government': ['cafr:NameOfGovernment']}
+    '''
+    df = pd.read_csv(path)
+    return {row[0] : row[1].split(';') for row in df.itertuples(index=False)}
+
+def configure_data(data, fields):
+    ''' Given a dictionary of data and a dictionary of configuration info, returns dictionary conforming to the configuration. '''
+    configured = {}
+
+    # Determine how many entries each column must have, for missing fields.
+    #row_count = len(data.values[0])
+    
+    # TODO: Support multiple input fields -- need more info on what should happen.
+    for name, input_fields in fields.items():
+        for field in input_fields:
+            if field in data:
+                configured[name] = data[field]
+    return configured
 
 
 # This is a quick hack replacement for BeautifulSoup, to work around whatever problem we're having there.
@@ -56,31 +81,6 @@ def tags_from_html(name, html):
         tag['attributes'] = atts
         tags.append(tag)
     return tags
-
-
-# ## Context definitions
-# Need to get a description for each context. A context element looks like this:
-# 
-#        <xbrli:context id="_ctx9">
-#           <xbrli:entity><xbrli:identifier scheme="http://www.govwiki.info">47210100100000</xbrli:identifier></xbrli:entity>
-#           <xbrli:period><xbrli:instant>2018-06-30</xbrli:instant></xbrli:period>
-#           <xbrli:scenario>
-#              <xbrldi:explicitMember dimension="cafr:FinancialReportingEntityAxis">cafr:PrimaryGovernmentActivitiesMember</xbrldi:explicitMember>
-#              <xbrldi:explicitMember dimension="cafr:BasisOfAccountingAxis">cafr:ModifiedAccrualBasisOfAccountingMember</xbrldi:explicitMember>
-#              <xbrldi:explicitMember dimension="cafr:ActivityTypeAxis">cafr:GovernmentalTypeActivityMember</xbrldi:explicitMember>
-#              <xbrldi:explicitMember dimension="cafr:ClassificationOfFundTypeAxis">cafr:GeneralFundMember</xbrldi:explicitMember>
-#              <xbrldi:explicitMember dimension="cafr:MagnitudeAxis">cafr:MajorMember</xbrldi:explicitMember>
-#           </xbrli:scenario>
-#        </xbrli:context>
-# 
-
-# ## Actual data
-# Data looks like this:
-# 
-#         <td id="_NETPOSITION_B10" style="text-align:right;width:114px;">$&#160;&#160;&#160;&#160;&#160;&#160;&#160;<ix:nonFraction name="cafr:CashAndCashEquivalents" contextRef="_ctx3" id="NETPOSITION_B10" unitRef="ISO4217_USD" decimals = "0" format="ixt:numdotdecimal">336,089,928</ix:nonFraction>&#160;</td>
-
-# In[33]:
-
 
 class XbrliDocument:
     def __init__(self, path = None, url = None):
@@ -103,7 +103,8 @@ class XbrliDocument:
             members = []
             for member in tags_from_html('xbrldi:explicitMember', tag['content']):
                 try:
-                    members.append(member['content'].replace('cafr:', '').replace('Member', ''))
+                    #members.append(member['content'].replace('cafr:', '').replace('Member', ''))
+                    members.append(member['content'])
                     members.sort()
                 except:
                     pass
@@ -122,7 +123,8 @@ class XbrliDocument:
                     # If there is description text, put it in parenthesis (if empty string, no parenthesis).
                     if description: description = f' ({description})'
 
-                    name = tag['attributes']['name'].replace('cafr:', '')
+#                    name = tag['attributes']['name'].replace('cafr:', '')
+                    name = tag['attributes']['name']
                     text = tag['content']
 
                     ix_fields[f'{name}{description}'] = text 
@@ -131,39 +133,66 @@ class XbrliDocument:
         return ix_fields
 
 
+def main(paths=None):
+    ''' For development, pass a list of paths and urls will be skipped. '''
+    fields = {}
+    try:
+        fields = config_fields()
+        print(f'Config file (config.csv) found.')
+    except:
+        print(f'Config file (config.csv) not found, will output all fields as found in documents.')
+
+    data = {}
+    docs = []
+    
+    if paths:
+        for path in paths:
+            print(f'Loading {path}...')
+            doc = XbrliDocument(path=path)
+            docs.append(doc)  
+    else:
+        for url in urls:
+            print(f'Downloading {url}...')
+            doc = XbrliDocument(url=url)
+            docs.append(doc)
+
+    # Because docs can have missing fields, and for the spreadsheet all docs must have entries for all fields,
+    # first need to figure out what all the fields from all the docs are, before processing the data.
+    for doc in docs:
+        for key in doc.ix_fields:
+            data.setdefault(key, [])
+
+    # Now can process the docs.
+    for doc in docs:
+        for key, value in doc.ix_fields.items():
+            data_list = data.setdefault(key, [])
+            data_list.append(value)
+
+        # And finally add blank entries for any missing fields.
+        for key, value in data.items():
+            if key not in doc.ix_fields:
+                value.append('')
+    
+    if fields:
+        data = configure_data(data, fields)
+
+    # Use Pandas to turn data dictionary into Excel
+    df = pd.DataFrame(data=data, dtype=int)
+    xlWriter = pd.ExcelWriter('output.xlsx', engine='xlsxwriter',options={'strings_to_numbers': True})
+    df.to_excel(xlWriter, index=False, sheet_name='CAFR Data')
+
+    print(f"Processed data for {len(urls)} entities, with a total of {len(data)} fields. See output.xlsx.")
+
 # In[34]:
 
 
-data = {}
-docs = []
-for url in urls:
-    print(f'Downloading {url}...')
-    doc = XbrliDocument(url=url)
-    docs.append(doc)
-
-print(f"DEBUG: Found {len(docs)} docs")
-
-# Because docs can have missing fields, and for the spreadsheet all docs must have entries for all fields,
-# first need to figure out what all the fields from all the docs are, before processing the data.
-for doc in docs:
-    for key in doc.ix_fields:
-        data.setdefault(key, [])
-
-# Now can process the docs.
-for doc in docs:
-    for key, value in doc.ix_fields.items():
-        data_list = data.setdefault(key, [])
-        data_list.append(value)
-
-    # And finally add blank entries for any missing fields.
-    for key, value in data.items():
-        if key not in doc.ix_fields:
-            value.append('')
-
-# Use Pandas to turn data dictionary into csv.
-df = pd.DataFrame(data)
-#df.to_csv('output.csv', index=False)
-df.to_excel('output.xlsx', index=False)
-
-print(f"Processed data for {len(urls)} entities, with a total of {len(data)} fields. See output.xlsx.")
-
+def test():
+    from mydir import mydir
+    from pathlib import Path
+    
+    paths = [str(path) for path in Path('test_data').iterdir()]
+    main()
+    #main(paths)
+	
+#main()
+test()
